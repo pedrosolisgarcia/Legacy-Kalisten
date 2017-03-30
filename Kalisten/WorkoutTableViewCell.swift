a369ee3e696044e942773b6fc654dc5878101f05
Pedro Solís García <solisgpedro@gmail.com>
2017-03-30 20:10:49 +0200

    Removed unnecessary class ViewController.swift. Added classes Wrokout

---
+++ Kalisten/WorkoutTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WorkoutTableViewCell.swift
+//  Kalisten
+//
+//  Created by Pedro Solís García on 30/03/17.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
