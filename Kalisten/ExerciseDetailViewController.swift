60e3a2ea57d587c7a3a709e7572440cd2f475d75
Pedro Solís García <solisgpedro@gmail.com>
2017-05-08 22:25:59 +0200

    Section selector in Detail Exercise now switches views. Preparing view Controller to support multiple tableViews

--- Kalisten/ExerciseDetailViewController.swift
+++ Kalisten/ExerciseDetailViewController.swift
@@ -12,14 +12,55 @@ class ExerciseDetailViewController: UIViewController, UITableViewDataSource, UIT
     
     @IBOutlet var exerciseImageView: UIImageView!
     
-    @IBOutlet var tableView: UITableView!
+    @IBOutlet weak var tableViewInformation: UITableView!
+    @IBOutlet weak var tableViewExecution: UITableView!
+    @IBOutlet weak var tableViewTarjects: UITableView!
     
     var exercise: Exercise!
+    
+    @IBOutlet var exerciseSections: UISegmentedControl!
+    
+    @IBOutlet weak var informationView: UIView!
+    @IBOutlet weak var executionView: UIView!
+    @IBOutlet weak var tarjetsView: UIView!
+    
+    //Switch between views when clicking the segments
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch exerciseSections.selectedSegmentIndex {
+        case 0:
+            informationView.isHidden = false
+            executionView.isHidden = true
+            tarjetsView.isHidden = true
+        case 1:
+            informationView.isHidden = true
+            executionView.isHidden = false
+            tarjetsView.isHidden = true
+        case 2:
+            informationView.isHidden = true
+            executionView.isHidden = true
+            tarjetsView.isHidden = false
+        default:
+            break;
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        //Initialize the tableViews in separate ways
+        tableViewInformation.dataSource = self
+        tableViewInformation.delegate = self
+        tableViewInformation.register(UITableViewCell.self, forCellReuseIdentifier: "informationCell")
+        
+        tableViewExecution.dataSource = self
+        tableViewExecution.delegate = self
+        tableViewExecution.register(UITableViewCell.self, forCellReuseIdentifier: "executionCell")
+        
+        tableViewTarjects.dataSource = self
+        tableViewTarjects.delegate = self
+        tableViewTarjects.register(UITableViewCell.self, forCellReuseIdentifier: "tarjectsCell")
         
         // Load image in the detail view
         exerciseImageView.image = UIImage()
@@ -37,12 +78,15 @@ class ExerciseDetailViewController: UIViewController, UITableViewDataSource, UIT
                 }
             })
         }
-
         
+        informationView.isHidden = false
+        executionView.isHidden = true
+        tarjetsView.isHidden = true
+
         //Set the default dimension of the cells.
-        tableView.estimatedRowHeight = 25
+        tableViewInformation.estimatedRowHeight = 25
         //The height of the cell will change according to the text length
-        tableView.rowHeight = UITableViewAutomaticDimension
+        tableViewInformation.rowHeight = UITableViewAutomaticDimension
         
         //Sets the header of the navigation bar to the exercise's name
         title = exercise.name.uppercased()
@@ -61,58 +105,79 @@ class ExerciseDetailViewController: UIViewController, UITableViewDataSource, UIT
         // Dispose of any resources that can be recreated.
     }
     
+    //Sets the number of rows of each tableView
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        
+        var count:Int?
+        
+        if tableView == self.tableViewInformation {
+            count = 5
+        }
+        
+        if tableView == self.tableViewExecution {
+            count =  4
+        }
+        
+        if tableView == self.tableViewTarjects {
+            count =  3
+        }
+        
+        return count!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ExerciseDetailTableViewCell
         
-        // Configure the cell...
-        switch indexPath.row{
-        case 0:
-            cell.fieldLabel.text = "NAME"
-            cell.valueLabel.text = exercise.name.uppercased()
-        case 1:
-            cell.fieldLabel.text = "FAMILY"
-            let arrayFamily:NSArray = exercise.family as NSArray
-            cell.valueLabel.text = arrayFamily.componentsJoined(by: ", ").uppercased()
-        case 2:
-            cell.fieldLabel.text = "PLACE"
-            let arrayPlace:NSArray = exercise.place as NSArray
-            cell.valueLabel.text = arrayPlace.componentsJoined(by: ", ").uppercased()
-        case 3:
-            cell.fieldLabel.text = "PHYSICAL QUALITY"
-            let arrayPQ:NSArray = exercise.pq as NSArray
-            cell.valueLabel.text = arrayPQ.componentsJoined(by: ", ").uppercased()
-        case 4:
-            cell.fieldLabel.text = "TARJETS"
-            let arrayTarjets:NSArray = exercise.tarjets as NSArray
-            cell.valueLabel.text = arrayTarjets.componentsJoined(by: ", ").uppercased()
-        default:
-            cell.fieldLabel.text = ""
-            cell.valueLabel.text = ""
+        var cell: ExerciseDetailTableViewCell!
+        
+        if tableView == self.tableViewInformation {
+            cell = tableView.dequeueReusableCell(withIdentifier: "informationCell", for: indexPath) as! ExerciseDetailTableViewCell
+            
+            // Configure the cell...
+            switch indexPath.row{
+            case 0:
+                cell.fieldLabel.text = "NAME"
+                cell.valueLabel.text = exercise.name.uppercased()
+            case 1:
+                cell.fieldLabel.text = "FAMILY"
+                let arrayFamily:NSArray = exercise.family as NSArray
+                cell.valueLabel.text = arrayFamily.componentsJoined(by: ", ").uppercased()
+            case 2:
+                cell.fieldLabel.text = "PLACE"
+                let arrayPlace:NSArray = exercise.place as NSArray
+                cell.valueLabel.text = arrayPlace.componentsJoined(by: ", ").uppercased()
+            case 3:
+                cell.fieldLabel.text = "PHYSICAL QUALITY"
+                let arrayPQ:NSArray = exercise.pq as NSArray
+                cell.valueLabel.text = arrayPQ.componentsJoined(by: ", ").uppercased()
+            case 4:
+                cell.fieldLabel.text = "TARJETS"
+                let arrayTarjets:NSArray = exercise.tarjets as NSArray
+                cell.valueLabel.text = arrayTarjets.componentsJoined(by: ", ").uppercased()
+            default:
+                cell.fieldLabel.text = ""
+                cell.valueLabel.text = ""
+            }
+            
+            cell.backgroundColor = UIColor.clear
+            
+            tableView.tableFooterView = UIView(frame: CGRect.zero)
+            
+            tableView.separatorColor = UIColor(red: 0/255, green: 114/255, blue: 206/255, alpha: 1)
+
+            
         }
         
-        cell.backgroundColor = UIColor.clear
+        if tableView == self.tableViewExecution {
+            cell = tableView.dequeueReusableCell(withIdentifier: "executionCell", for: indexPath) as! ExerciseDetailTableViewCell
+            
+        }
         
-        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        if tableView == self.tableViewTarjects {
+            cell = tableView.dequeueReusableCell(withIdentifier: "tarjectsCell", for: indexPath) as! ExerciseDetailTableViewCell
+            
+        }
         
-        tableView.separatorColor = UIColor(red: 0/255, green: 114/255, blue: 206/255, alpha: 1)
-        
-        return cell
+        return cell!
     }
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
