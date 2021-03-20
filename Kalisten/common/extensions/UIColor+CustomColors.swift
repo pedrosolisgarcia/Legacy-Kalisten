a51f4d9b542a890f6b90c5e49e3e34af01373b4c
Pedro Solis <pedro.solis@tigerspike.com>
2021-03-20 16:36:23 +0100

    Removed SwiftUI from the original UIKit version

--- Kalisten/common/extensions/UIColor+CustomColors.swift
+++ Kalisten/common/extensions/UIColor+CustomColors.swift
@@ -1,5 +1,4 @@
 import UIKit
-import SwiftUI
 
 extension UIColor {
   static let availableGreen = UIColor(hex: 0x64D232)
@@ -12,21 +11,4 @@ extension UIColor {
   static let darkenGrey = UIColor(hex: 0x191919)
   static let kalistenBlack = UIColor(hex: 0x212121)
   static let kalistenDarkenBlack = UIColor(hex: 0x1F1F1F)
-}
-
-extension Color {
-  static let availableGreen = Color(hex: "64D232")
-  static let errorRed = Color(hex: "FA3232")
-  static let estonianBlue = Color(hex: "0072CE")
-  static let lightGrey = Color(hex: "CCCCCC")
-  static let midGrey = Color(hex: "AAAAAA")
-  static let spaceGrey = Color(hex: "1F2124")
-  static let darkGrey = Color(hex: "151515")
-  static let darkenGrey = Color(hex: "191919")
-  static let kalistenBlack = Color(hex: "212121")
-  static let kalistenDarkenBlack = Color(hex: "1F1F1F")
-  static let kalistenDarkGray = Color(hex: "333333")
-  static let kalistenLightGray = Color(hex: "999999")
-  static let kalistenClearGray = Color(hex: "d9d9d9")
-  static let kalistenGray = Color(hex: "666666")
 }
